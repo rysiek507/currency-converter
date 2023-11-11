c52d7c3f4d82d5403cd9491afedf1394e50b4712
Radoslaw Siek <radosaw.siek@gmail.com>
2023-11-11 19:55:52 +0100

    change function to array function

--- js/script.js
+++ js/script.js
@@ -1,24 +1,46 @@
-function count() {
-  const formElement = document.querySelector(".js-form");
-  const countElement = document.querySelector(".js-count");
-  const currencyElement = document.querySelector(".js-currency");
-  const resultElement = document.querySelector(".js-result");
-  formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+{
+  const countSum = (currencyElement, countElement) => {
+    const rateEUR = 4.64;
+    const rateUSD = 4.31;
     switch (currencyElement.value) {
       case "eur":
-        resultElement.innerText = `Za ${countElement.value} euro dostaniesz ${(
-          +countElement.value * 4.64
-        ).toFixed(2)} zł`;
+        return (+countElement.value * rateEUR).toFixed(2);
+      case "usd":
+        return (+countElement.value * rateUSD).toFixed(2);
+      default:
+        return;
+    }
+  };
+
+  const writeAnswer = (
+    currencyElement,
+    resultElement,
+    countElement,
+    answer
+  ) => {
+    switch (currencyElement.value) {
+      case "eur":
+        resultElement.innerText = `Za ${countElement.value} euro dostaniesz ${answer} zł`;
         break;
       case "usd":
-        resultElement.innerText = `Za ${countElement.value} usd dostaniesz ${(
-          +countElement.value * 4.31
-        ).toFixed(2)} zł`;
+        resultElement.innerText = `Za ${countElement.value} usd dostaniesz ${answer} zł`;
         break;
       default:
         resultElement.innerText = "Podano złą walutę";
     }
-  });
+  };
+
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const countElement = document.querySelector(".js-count");
+      const currencyElement = document.querySelector(".js-currency");
+      const resultElement = document.querySelector(".js-result");
+      let answer = countSum(currencyElement, countElement);
+      writeAnswer(currencyElement, resultElement, countElement, answer);
+    });
+  };
+
+  init();
 }
-count();
